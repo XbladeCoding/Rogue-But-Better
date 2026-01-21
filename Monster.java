96d6f6a848275cecb5d8ded2a6916624581f21e0
kavithago_microsoft <kavithago_mnicrosoft.com>
2026-01-20 22:03:34 -0500

    Remove all compiled .class files from repository

--- Monster.java
+++
@@ -1,9 +0,0 @@
-public class Monster {
-    public final Game game;
-    public final Dungeon dungeon;
-
-    public Monster(Game g) {
-        this.game = g;
-        this.dungeon = g.getDungeon();
-    }
-}
